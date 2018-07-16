712bbaa43db0a0fbbf2f4f15fe733c982b4ee40f
Zaharov Nikolay <zaharovkoly@mail.ru>
2018-07-16 17:52:51 +0300

    Initial commit

---
+++ app/src/main/java/zakharov/nikolay/com/androidtabsexample/TabFragment2.java
@@ -0,0 +1,19 @@
+package zakharov.nikolay.com.androidtabsexample;
+
+/**
+ * Created by user on 16.07.2018.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+    }
+}
